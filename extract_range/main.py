7160d92fd4aa5a12cee6a0b1320a9fcf81f1021c
mpbar <mpbarbel@colostate.edu>
2024-02-08 02:25:35 -0700

    extract_range

---
+++ extract_range/main.py
@@ -0,0 +1,8 @@
+# extract values from a sorted list
+
+import math # optional and you can delete this line if not useful
+
+# subroutines if any, go here
+
+# your subroutine goes here
+def extract(list,lo,hi):
